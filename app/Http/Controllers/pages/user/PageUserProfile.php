ce88cef33368554c316f120a144ce591fbab266d
eren <64080210+erenkarakoc@users.noreply.github.com>
2025-01-03 23:09:25 +0300

    a

--- app/Http/Controllers/pages/user/PageUserProfile.php
+++ app/Http/Controllers/pages/user/PageUserProfile.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\pages\user;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Webpatser\Countries\Countries;
 
 class PageUserProfile extends Controller
 {
@@ -15,12 +14,10 @@ class PageUserProfile extends Controller
      */
     public function show(Request $request)
     {
-        $countries = Countries::all();
 
         return view('content.pages.user.page-user-profile', [
             'request' => $request,
             'user' => $request->user(),
-            'countries' => $countries,
         ]);
     }
 }
