61db0d9422615491500406b91be190e1b73436ff
eren <64080210+erenkarakoc@users.noreply.github.com>
2025-03-02 09:54:38 +0300

    a

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel {
         Commands\Blockchain\TronCheckWallets::class,
         Commands\SwapCheck\SwapTronCheck::class,
         Commands\LockedPacks\CheckLockedPacks::class,
+        Commands\LockedPacks\CheckPendingTradeTransactions::class,
         Commands\UpdateMarketData::class,
         Commands\SeedCountries::class,
         Commands\SeedAlgorithms::class,
